87f256f73adbb321dc534af73a615c91b8295fbd
shirashun0409 <shun.shiramatsu@gmail.com>
2026-09-06 18:01:03 +0900

    Title

--- Assets/Scripts/Logo Rotation.cs
+++ Assets/Scripts/Logo Rotation.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
 
-
-public class LogoRotate : MonoBehaviour
+public class LogoSoftRotate : MonoBehaviour
 {
+    public float angle = 1f;   // ほんの少しだけ角度が変わる
+    public float speed = 0.5f; // ゆっくり
+
+    private Quaternion startRot;
+
+    void Start()
+    {
+        startRot = transform.localRotation;
+    }
+
     void Update()
     {
-        transform.Rotate(0, 20 * Time.deltaTime, 0);
+        float rot = Mathf.Sin(Time.time * speed) * angle;
+        transform.localRotation = startRot * Quaternion.Euler(0, rot, 0);
     }
 }
