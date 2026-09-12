f8d11ba12b038f271c11453ca992a63b00ae8494
shirashun0409 <shun.shiramatsu@gmail.com>
2026-09-12 17:44:02 +0900

    a

---
+++ Assets/Scripts/KanjiDisplay.cs
@@ -0,0 +1,13 @@
+uusing UnityEngine;
+using TMPro;
+
+public class KanjiDisplay : MonoBehaviour
+{
+    [SerializeField] private TMP_Text idiomText; // 生物 とか表示する TextMeshPro
+
+    // 熟語が成立したときに呼ばれる関数
+    public void SetIdiom(string idiom)
+    {
+        idiomText.text = idiom;
+    }
+}
